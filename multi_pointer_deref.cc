70493c64cfdaa30a5d1f6d507a99ec05a2ac2e2a
aodong.qin <ioperations.c@gmail.com>
2023-07-30 22:42:36 +0800

    add usdt to multi_pointer_deref.cc cause its too difficult for stap to decode

--- multi_pointer_deref.cc
+++ multi_pointer_deref.cc
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#include <string>
+
+#include <sys/sdt.h>
+
 struct test {
     const char* a;
     int b;
@@ -8,6 +12,8 @@ struct test {
 static void func(const char* pstr,const char** ppstr, struct test* pt,
                  struct test** ppt, struct test*** pppt) {
     printf("pstr: %s, *ppstr: %s\n", pstr, *ppstr);
+    std::string s (pstr);
+    STAP_PROBE2(m, func, s.c_str(), pstr);
     printf("pt->a: %s, pt->b: %d: \n", pt->a, pt->b);
     printf("(*ppt)->a: %s, (*ppt)->b: %d: \n", (*ppt)->a, (*ppt)->b);
     printf("(**pppt)->a: %s, (**pppt）->b: %d: \n", (**pppt)->a, (**pppt)->b);
