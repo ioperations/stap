1da086f2e69639987cf1cb6ff170fac251cf47e3
aodong.qin <ioperations.c@gmail.com>
2023-08-13 14:37:32 +0800

    [lru] add more elements

--- lru.cc
+++ lru.cc
@@ -45,15 +45,15 @@ class LRUCache final {
     }
 
     ~LRUCache() {
-        Node *p = head;
+        Node* p = head;
         while (p) {
-            Node *q = p;
+            Node* q = p;
             p = p->next;
             delete q;
         }
     }
 
-    Optional<V> Get(const K &key) {
+    Optional<V> Get(const K& key) {
         if (map.find(key) != map.end()) {
             DelNode(map[key]);
             AddToFirstNode(map[key]);
@@ -62,13 +62,13 @@ class LRUCache final {
         return Optional<V>();
     }
 
-    void Set(const K &key, const V &value) {
+    void Set(const K& key, const V& value) {
         if (map.find(key) != map.end()) {
             map[key]->value = value;
             DelNode(map[key]);
             AddToFirstNode((map[key]));
         } else {
-            Node *p = new Node();
+            Node* p = new Node();
             p->key = key;
             p->value = value;
 
@@ -84,24 +84,24 @@ class LRUCache final {
     struct Node {
         K key;
         V value;
-        Node *pre;
-        Node *next;
+        Node* pre;
+        Node* next;
     };
 
-    void AddToFirstNode(Node *node) {
+    void AddToFirstNode(Node* node) {
         head->next->pre = node;
         node->pre = head;
         node->next = head->next;
         head->next = node;
     }
 
-    void DelNode(Node *node) {
+    void DelNode(Node* node) {
         node->pre->next = node->next;
         node->next->pre = node->pre;
     }
 
     void DelLastNode() {
-        Node *p = tail->pre;
+        Node* p = tail->pre;
 
         tail->pre = p->pre;
         p->pre->next = tail;
@@ -110,22 +110,27 @@ class LRUCache final {
         delete p;
     }
 
-    Node *head;
-    Node *tail;
-    std::map<K, Node *> map;
+    Node* head;
+    Node* tail;
+    std::map<K, Node*> map;
     int capability;
 };
 
-
-int main(int argc, char *argv[]) {
-    LRUCache<int, int> lru(3);
+int main(int argc, char* argv[]) {
+    LRUCache<int, int> lru(10);
     lru.Set(10, 10);
     lru.Set(1, 2);
     lru.Set(2, 3);
     lru.Set(3, 4);
+    lru.Set(4, 5);
+    lru.Set(5, 6);
+    lru.Set(6, 7);
+    lru.Set(7, 8);
+    lru.Set(8, 9);
+    lru.Set(9, 10);
 
     auto ret = lru.Get(10);
-    assert(!ret);
+    assert(ret);
     ret = lru.Get(1);
     assert(ret == 2);
     return 0;
